d8bfd914951c7df482b56d812f9d143b855a5e53
Daniel Victor <danielvictor@dialhost.com.br>
2019-03-28 14:52:08 -0300

    #3 Ajustes na implementação

---
+++ app/Http/Controllers/BaseController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\JobsController;
+use App\Http\Controllers\Auth;
+
+class BaseController extends Controller
+{
+    
+    public function JobsGetAll(JobsController $jobs){
+        return $jobs->index();
+    }
+}
